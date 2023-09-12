36830b69fec9827e5db580f7bc9204f59b7c2c17
ohenry0227 <71479006+ohenry0227@users.noreply.github.com>
2023-09-12 14:35:18 +0530

    Add files via upload
    To reinitialise the YUM in centos 8 stream. download and execute before running any yum command

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /etc/yum.repos.d/
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
+timedatectl set-timezone "Asia/Kolkata"
+
+yum install lsof
+yum install telnet
+yum install ncurses
+
+
+
